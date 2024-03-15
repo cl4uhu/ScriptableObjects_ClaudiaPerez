4babb0cae6cd960b4e03da26fc47d2fec1204915
cl4uhu <pjclaudia1@gmail.com>
2024-03-15 10:57:43 +0100

    inventory manager, window and slot

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -10,6 +10,7 @@ public class InventoryManager : MonoBehaviour
     public ScriptableItem[] weapons;
     public Text[] weaponsNames;
     public Image[] weaponsSprites; 
+    public InventorySlot[] weaponSlots;
 
     void Awake()
     {
@@ -25,6 +26,11 @@ public class InventoryManager : MonoBehaviour
                 weapons[i] = item; 
                 weaponsNames[i].text = item.itemName;
                 weaponsSprites[i].sprite = item.itemSprite;
+
+                weaponSlots[i].slotItem = item; 
+                weaponSlots[i].slotNumber = i;
+
+                return;
             }
         }
     }
