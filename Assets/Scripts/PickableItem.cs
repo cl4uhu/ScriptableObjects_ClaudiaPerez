54f7e0a952e0f231d3bef5e553cc743cd098eb9f
cl4uhu <pjclaudia1@gmail.com>
2024-03-15 09:56:30 +0100

    scriptable objects

---
+++ Assets/Scripts/PickableItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableItem : MonoBehaviour
+{
+    public ScriptableItem item; 
+    private SpriteRenderer spriteRenderer; 
+
+    void Awake()
+    {
+       spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+    
+    void Start()
+    {
+        spriteRenderer.sprite = item.itemSprite;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/ScriptableItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Items/New Item")]
+
+public class ScriptableItem : ScriptableObject
+{
+   public enum ItemType
+   {
+     Weapon,
+     Armor,
+     Shield
+   }
+
+   public ItemType itemType;
+
+   public string itemName; 
+   public string itemDescription;
+   public string itemPrice;
+   public Sprite itemSprite;
+
+}
